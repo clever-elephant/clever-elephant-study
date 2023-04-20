f9824e5b698412cacd9c3bbc553175bb966e2ed8
wangzida <819971207@qq.com>
2023-04-20 14:50:33 +0800

    servlet配置完成

--- spring-core/src/main/java/com/ce/mvcframework/v1/servlet/DispatcherServlet.java
+++ spring-core/src/main/java/com/ce/mvcframework/v1/servlet/DispatcherServlet.java
@@ -1,4 +1,56 @@
 package com.ce.mvcframework.v1.servlet;
 
-public class DispatcherServlet {
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class DispatcherServlet extends HttpServlet {
+
+    private Map<String, Object> mapping = new HashMap<>();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+
+        try (InputStream is = this.getClass().getClassLoader().getResourceAsStream(config.getInitParameter("contextConfigLocation"))) {
+            Properties configContext = new Properties();
+            configContext.load(is);
+            String scanPackage = configContext.getProperty("scanPackage");
+            doScanner(scanPackage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        super.init();
+    }
+
+    private void doScanner(String scanPackage) {
+        URL url = this.getClass().getClassLoader().getResource(scanPackage.replaceAll("\\.", "/"));
+        File classDir = new File(url.getFile());
+        for (File file : classDir.listFiles()) {
+            if (file.isDirectory()) {
+                doScanner(scanPackage + "." + file.getName());
+            } else {
+                String clazzName = scanPackage + "." + file.getName().replace(".class", "");
+                mapping.put(clazzName, null);
+            }
+        }
+    }
 }
